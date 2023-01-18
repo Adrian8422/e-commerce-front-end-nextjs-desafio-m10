294130381bfbee322816d9af0b6177ab8239100c
Adrian Villegas <manbassman1996@gmail.com>
2023-01-18 09:53:52 -0300

    changes

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = withSvgr({
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
     styledComponents: true,
   },
+  experimental: {
+    images: {
+      layoutRaw: true,
+    },
+  },
 });
 
 module.exports = nextConfig;
